4441a4b03d34a3b9a1f849ad621c5313ce5ccd2c
Aseni <alusinejalloh800@gmail.com>
2022-08-07 22:22:24 +0300

    add react front end structure

---
+++ src/front-end/src/App.js
@@ -0,0 +1,12 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+     <div>
+      <h1>Hello from SL Student Community in Cyprus</h1>
+     </div>
+  );
+}
+
+export default App;
