406ba71b3ab6432ce6b26652b089f93909410e41
Aseni <alusinejalloh800@gmail.com>
2022-08-07 17:48:27 +0300

    Add Project Structure Set_up

---
+++ src/main/java/com/aj/sl_student_community_cyprus/SlStudentCommunityCyprusApplication.java
@@ -0,0 +1,13 @@
+package com.aj.sl_student_community_cyprus;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SlStudentCommunityCyprusApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SlStudentCommunityCyprusApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/aj/sl_student_community_cyprus/SlStudentCommunityCyprusApplicationTests.java
@@ -0,0 +1,13 @@
+package com.aj.sl_student_community_cyprus;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SlStudentCommunityCyprusApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
