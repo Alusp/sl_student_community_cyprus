233e5a09c3bb7b71ee5413c03fa93c583f433068
Aseni <alusinejalloh800@gmail.com>
2022-08-07 23:33:53 +0300

    Wire client .js into app .js

--- src/front-end/src/App.js
+++ src/front-end/src/App.js
@@ -1,12 +1,55 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import { getAllStudents } from "./client";
+import { Component } from "react";
 
-function App() {
-  return (
-     <div>
-      <h1>Hello from SL Student Community in Cyprus</h1>
-     </div>
-  );
+class App extends Component {
+  state = {
+    students: [],
+  };
+
+  componentDidMount() {
+    this.fetchStudents();
+  }
+
+  fetchStudents = () => {
+    getAllStudents().then((res) =>
+      res.json().then((students) => {
+        console.log(students);
+         this.setState({
+          students
+         });
+      })
+    );
+  };
+
+  render() {
+  
+    const { students } = this.state;
+    if(students && students.length){
+     return students.map((students, index) => {
+        return(
+          <div key={index}>
+            <h2>
+              {students.studentId}
+            </h2>
+            <p>
+              {students.firstName}
+            </p>
+            <p>
+              {students.lastName}
+            </p>
+            <p>
+              {students.email}
+            </p>
+            <p>
+              {students.gender}
+            </p>
+          </div>
+         )
+      })
+    }
+    return <h1> No Students found</h1>;
+  }
 }
 
 export default App;
---
+++ src/front-end/src/client.js
@@ -0,0 +1,3 @@
+import fetch from 'unfetch';
+
+export const getAllStudents = () => fetch('/students');
