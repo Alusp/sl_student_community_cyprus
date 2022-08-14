c1f674ab137bcd0260871db2881582899bc61592
Aseni <alusinejalloh800@gmail.com>
2022-08-14 15:26:02 +0300

    Add configurations for DataSource

---
+++ src/main/java/com/aj/sl_student_community_cyprus/datasource/Datasource.java
@@ -0,0 +1,18 @@
+package com.aj.sl_student_community_cyprus.datasource;
+
+import com.zaxxer.hikari.HikariDataSource;
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.boot.jdbc.DataSourceBuilder;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class Datasource {
+    @Bean
+    @ConfigurationProperties("app.datasource")
+    public HikariDataSource hikariDataSource() {
+        return DataSourceBuilder.create()
+                .type(HikariDataSource.class)
+                .build();
+    }
+}
