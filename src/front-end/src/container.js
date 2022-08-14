64088b24bfdcdf6fb288aa3d8c1051368cc0d21c
Aseni <alusinejalloh800@gmail.com>
2022-08-14 13:24:57 +0300

    Add Margin top to the Container

--- src/front-end/src/container.js
+++ src/front-end/src/container.js
@@ -1,7 +1,7 @@
 import React from "react";
 const Container = props => (
 
-  <div style={{width: '1400px', margin: '0 auto'}}>
+  <div style={{width: '1400px', margin: '0 auto', textAlign: 'center', marginTop: '120px'}}>
        {props.children}
   </div>
 );
